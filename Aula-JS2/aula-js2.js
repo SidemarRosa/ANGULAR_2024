37431b356aefb8225b338a2301a7dfd5b5c711a2
SidemarRosa <sidr.com.slr@gmail.com>
2024-02-22 16:37:11 -0300

    condição - if else

--- Aula-JS2/aula-js2.js
+++ Aula-JS2/aula-js2.js
@@ -3,10 +3,14 @@ const camisetaSideAzul = true;
 const camisetaSideVermelha = false;
 */
 
-
 //condicional booleana
-const numero = 10;
+const numero = 0;
 const numeroPar = numero % 2 === 0;
+console.log(numeroPar);
 
-
-
+//condição if e else
+if (numeroPar) {
+  console.log("É par");
+} else {
+  console.log("Não é Par");
+}
