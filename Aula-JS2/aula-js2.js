765ce9d9e6598504c86060fce9d0fbfc957c3905
SidemarRosa <sidr.com.slr@gmail.com>
2024-02-22 16:51:01 -0300

    Aula 2 - Else if

--- Aula-JS2/aula-js2.js
+++ Aula-JS2/aula-js2.js
@@ -2,15 +2,22 @@
 const camisetaSideAzul = true;
 const camisetaSideVermelha = false;
 */
-
+/*condição if e else
+if (numeroPar) {
+    console.log("É par");
+} else if {
+    console.log("Não é Par");
+}*/
 //condicional booleana
 const numero = 0;
-const numeroPar = numero % 2 === 0;
-console.log(numeroPar);
+const numeroDivisivelPor5 = numero % 5 === 0;
+console.log(numeroDivisivelPor5);
 
-//condição if e else
-if (numeroPar) {
-  console.log("É par");
+//Else IF
+if (numeroDivisivelPor5 === 0) {
+  console.log("Numeor Invalido");
+} else if (numeroDivisivelPor5) {
+  console.log("Sim");
 } else {
-  console.log("Não é Par");
+  console.log("Não");
 }
